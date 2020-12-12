b3bfc04a44155d99fc8fad92c2f3d7228f186ab9
aanper <mail@s3f.ru>
2020-12-12 11:38:59 +0300

    merge

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const glsl = x => x;
+const vert = x => x;
+const frag = x => x;
+
 const vsSource = `
     void main() {
         gl_Position = vec4(0, 0, 0, 1);
@@ -37,17 +41,7 @@ function loadShader(gl, type, source) {
     return shader;
 }
 
-function main() {
-    const canvas = document.querySelector("#glCanvas");
-    // Initialize the GL context
-    const gl = canvas.getContext("webgl");
-  
-    // Only continue if WebGL is available and working
-    if (gl === null) {
-        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-        return;
-    }
-
+function draw(gl) {
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
     const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
 
@@ -69,5 +63,66 @@ function main() {
     // Clear the color buffer with specified clear color
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
+
+function draw2(gl) {
+    const vsGLSL = vert`#version 300 es
+        void main() {
+            gl_Position = vec4(0, 0, 0, 1);
+            gl_PointSize = 100.0;
+        }
+    `;
+
+    const fsGLSL = frag`#version 300 es
+        precision highp float;
+
+        out vec4 outColor;
+
+        void main() {
+            outColor = vec4(1, 0.5, 0, 1);
+        }
+    `;
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vsGLSL);
+    gl.compileShader(vertexShader);
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    throw new Error(gl.getShaderInfoLog(vertexShader))
+    };
+
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fsGLSL);
+    gl.compileShader(fragmentShader);
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    throw new Error(gl.getShaderInfoLog(fragmentShader))
+    };
+
+    const prg = gl.createProgram();
+    gl.attachShader(prg, vertexShader);
+    gl.attachShader(prg, fragmentShader);
+    gl.linkProgram(prg);
+    if (!gl.getProgramParameter(prg, gl.LINK_STATUS)) {
+    throw new Error(gl.getProgramInfoLog(prg))
+    };
+
+    gl.useProgram(prg);
+
+    // draw 1 point
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
+
+function main() {
+    const canvas = document.querySelector("#glCanvas");
+    // Initialize the GL context
+    const gl = canvas.getContext("webgl2");
   
-window.onload = main;  
+    // Only continue if WebGL is available and working
+    if (gl === null) {
+        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+        return;
+    }
+
+    draw2(gl);
+    // draw(gl);
+}
+  
+window.onload = main;
