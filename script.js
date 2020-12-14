551928304a8f2052d9c6f091b342421dcca04028
aanper <mail@s3f.ru>
2020-12-14 11:03:17 +0300

    cube

--- script.js
+++ script.js
@@ -74,10 +74,41 @@ function init(gl) {
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
         const positions = [
-            -1.0,  1.0,
-            1.0,  1.0,
-            -1.0, -1.0,
-            1.0, -1.0,
+            // Front face
+            -1.0, -1.0,  1.0,
+             1.0, -1.0,  1.0,
+             1.0,  1.0,  1.0,
+            -1.0,  1.0,  1.0,
+            
+            // Back face
+            -1.0, -1.0, -1.0,
+            -1.0,  1.0, -1.0,
+             1.0,  1.0, -1.0,
+             1.0, -1.0, -1.0,
+            
+            // Top face
+            -1.0,  1.0, -1.0,
+            -1.0,  1.0,  1.0,
+             1.0,  1.0,  1.0,
+             1.0,  1.0, -1.0,
+            
+            // Bottom face
+            -1.0, -1.0, -1.0,
+             1.0, -1.0, -1.0,
+             1.0, -1.0,  1.0,
+            -1.0, -1.0,  1.0,
+            
+            // Right face
+             1.0, -1.0, -1.0,
+             1.0,  1.0, -1.0,
+             1.0,  1.0,  1.0,
+             1.0, -1.0,  1.0,
+            
+            // Left face
+            -1.0, -1.0, -1.0,
+            -1.0, -1.0,  1.0,
+            -1.0,  1.0,  1.0,
+            -1.0,  1.0, -1.0,
         ];
 
         // fill buffer with positions
@@ -90,7 +121,7 @@ function init(gl) {
         const vertexPosition = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
         gl.vertexAttribPointer(
             vertexPosition,
-            2, // pull out 2 values per iteration
+            3, // pull out 3 values per iteration
             gl.FLOAT, // the data in the buffer is 32bit floats
             false,  // don't normalize
             // how many bytes to get from one set of values to the next
@@ -102,12 +133,23 @@ function init(gl) {
     }
 
     {
-        const colors = [
-            1.0,  1.0,  1.0,  1.0,    // white
-            1.0,  0.0,  0.0,  1.0,    // red
-            0.0,  1.0,  0.0,  1.0,    // green
-            0.0,  0.0,  1.0,  1.0,    // blue
+        const faceColors = [
+            [1.0,  1.0,  1.0,  1.0],    // Front face: white
+            [1.0,  0.0,  0.0,  1.0],    // Back face: red
+            [0.0,  1.0,  0.0,  1.0],    // Top face: green
+            [0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
+            [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
+            [1.0,  0.0,  1.0,  1.0],    // Left face: purple
         ];
+
+        var colors = [];
+        
+        for (var j = 0; j < faceColors.length; ++j) {
+            const c = faceColors[j];
+            
+            // Repeat each color four times for the four vertices of the face
+            colors = colors.concat(c, c, c, c);
+        }
 
         const colorBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
@@ -150,6 +192,19 @@ function init(gl) {
 
     const modelViewMatrixPointer = gl.getUniformLocation(shaderProgram, 'uModelViewMatrix');
 
+    const indices = [
+        0,  1,  2,      0,  2,  3,    // front
+        4,  5,  6,      4,  6,  7,    // back
+        8,  9,  10,     8,  10, 11,   // top
+        12, 13, 14,     12, 14, 15,   // bottom
+        16, 17, 18,     16, 18, 19,   // right
+        20, 21, 22,     20, 22, 23,   // left
+    ];
+    const indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+  
+
     return {
         shaderProgram,
         modelViewMatrix,
@@ -158,7 +213,7 @@ function init(gl) {
     }
 }
 
-let squareRotation = 2.0;
+let cubeRotation = 2.0;
 
 function draw(gl, ctx, dt) {
     gl.clearColor(0.0, 0.0, 0.0, 1.0); // Set clear color to black, fully opaque
@@ -168,8 +223,18 @@ function draw(gl, ctx, dt) {
 
     mat4.rotate(ctx.rotatedMatrix,  // destination matrix
         ctx.modelViewMatrix,  // matrix to rotate
-        squareRotation,   // amount to rotate in radians
+        cubeRotation,   // amount to rotate in radians
         [0, 0, 1]);       // axis to rotate around
+
+    mat4.rotate(ctx.rotatedMatrix,  // destination matrix
+        ctx.modelViewMatrix,  // matrix to rotate
+        cubeRotation,     // amount to rotate in radians
+        [0, 0, 1]);       // axis to rotate around (Z)
+    
+    mat4.rotate(ctx.rotatedMatrix,  // destination matrix
+        ctx.rotatedMatrix,  // matrix to rotate
+        cubeRotation * .7,// amount to rotate in radians
+        [0, 1, 0]);       // axis to rotate around (X)
 
     
     gl.uniformMatrix4fv(
@@ -177,12 +242,14 @@ function draw(gl, ctx, dt) {
         false,
         ctx.rotatedMatrix);
     
-    gl.drawArrays(gl.TRIANGLE_STRIP,
-        0, // offset
-        4, // vertexCount
+    gl.drawElements(
+        gl.TRIANGLES,
+        2 * 3 * 6, // vertexCount
+        gl.UNSIGNED_SHORT, // type
+        0 // offset
     );
 
-    squareRotation += dt;
+    cubeRotation += dt;
 }
 
 function main() {
